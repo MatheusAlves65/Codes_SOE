ca7ae13dbec3acbfb765c7ba2916d324dc2bde95
gabrielroblima <55054717+gabrielroblima@users.noreply.github.com>
2023-12-04 15:08:43 -0300

    Atualização

--- ex1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i {100..1}
-do
-nome_arquivo = "teste$(i).txt"
-texto = "Numero do arquivo = $(i)"
-echo "$texto" > $nome_arqui
- 
-done
--- testes/shell_script/ex31.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Nome do arquivo que contém as URLs
-arquivo_urls="sites.txt"
-
-# Verifica se o arquivo existe e faz o download dos arquivos
-[ -f "$arquivo_urls" ] && wget -i "$arquivo_urls"
